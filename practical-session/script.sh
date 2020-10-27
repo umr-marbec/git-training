1f506037752686ad94340f6b4ab764a9e52bea23
barrier <nicolas.barrier@ird.fr>
2020-10-27 09:28:59 +0100

    Update file

--- practical-session/script.sh
+++ practical-session/script.sh
@@ -1,16 +1,10 @@
-version="6"
+feat_label="1"
 
 # Creation of first feature branch
-git flow feature start feat${version}A
-touch feat-${version}A1.txt
-git add feat-${version}A1.txt 
-git commit -m "feat-${version}A1.txt" feat-${version}A1.txt 
-touch feat-${version}A2.txt
-git add feat-${version}A2.txt 
-git commit -m "feat-${version}A2.txt" feat-${version}A2.txt 
-git flow feature publish
-
-#git flow feature finish feat${version}B
-#git push
-#git flow feature checkout feat${version}A
-#git flow feature finish feat${version}A
+git flow feature start feat${feat_label}A
+touch feat-${feat_label}A.txt
+git add feat-${feat_label}A.txt 
+git commit -m "feat-${feat_label}A.txt" feat-${feat_label}A.txt 
+touch feat-${feat_label}B.txt
+git add feat-${feat_label}B.txt 
+git commit -m "feat-${feat_label}B.txt" feat-${feat_label}B.txt 
